d164383e2bc0d1a5a8a6340c2496cdc55016394b
blacknight0712 <blacknight0981@gmail.com>
2024-09-26 14:28:46 +0800

    調整 logger json 輸出格式

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,14 @@
 import { createLogger, format, transports } from "winston";
+import { inspect } from 'util';
 
 const { combine, timestamp, printf, colorize } = format;
 
 const customFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} [${level}]: ${message}`;
+    const formattedMessage = typeof message === 'object'
+        ? inspect(message, { depth: null, colors: true })
+        : message;
+
+    return `${timestamp} [${level}]: ${formattedMessage}`;
 });
 
 const logger = createLogger({
